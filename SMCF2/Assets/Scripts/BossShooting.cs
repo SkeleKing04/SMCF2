74faf3c1c3a04790737c075a52ee43ed0874005f
Jackson Lucas <jjlucas04@bigpond.com>
2021-09-03 12:20:22 +0930

    SinMovement
    Added SinDraw Script
    Added Testing Scene
    Bombs now aim at the player upon spawning
     - Need to add a bit of randomness to that (be a bit off from the player's pos)

--- SMCF2/Assets/Scripts/BossShooting.cs
+++ SMCF2/Assets/Scripts/BossShooting.cs
@@ -7,17 +7,26 @@ public class BossShooting : MonoBehaviour
     public float savedTime;
     public GameObject fireTransform;
     public Rigidbody[] bullet;
-    public enum BulletState
+    public enum Ammo
     {
         bomb,
         missile,
         bullet
     };
-    public BulletState bulletState;
+    public Ammo ammo;
+    public enum ShootingArangement{
+        Gattling,
+        Starfire,
+        Controlled
+    };
+    public ShootingArangement shootingArangement;
     public int bulletCount;
     public Vector3 spawn;
+    private Vector3 lookPos;
     private Rigidbody shellInstance;
     private GameObject player;
+    public float Speed;
+    public float Size;
     private bool canSpawn = true;
     // Start is called before the first frame update
     void Start()
@@ -28,33 +37,38 @@ public class BossShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(canSpawn && GameManager.GlobalTimer - savedTime >= 0.1)
+        if(canSpawn && GameManager.GlobalTimer - savedTime >= 0.01)
         {
             for(int i = 0; i < bulletCount; i++)
             {
-            switch (bulletState)
+            switch (ammo)
             {
-               case BulletState.bomb:
+               case Ammo.bomb:
                     //Debug.Log("Spawned Bomb");
-                    Rigidbody shellInstance = Instantiate(bullet[0], new Vector3(20 * Mathf.Cos(transform.rotation.y + (90* (Mathf.PI/180))), fireTransform.transform.position.y, 0 * Mathf.Sin(transform.rotation.y * 4)), fireTransform.transform.rotation) as Rigidbody;
+                    spawn = new Vector3((Mathf.Sin(Time.time * Speed) * Size), fireTransform.transform.position.y, (Mathf.Cos(Time.time * Speed) * Size));
+                    Debug.Log("Rotation " + transform.rotation.y);
+                    //lookPos = player.transform.position - spawn;
+                    //Debug.Log("LookPos - " + lookPos);
+                    //Quaternion.Slerp(rigidbody.transform.rotation, Quaternion.LookRotation(lookPos), Time.deltaTime * rotateSpeed);
+                    Rigidbody shellInstance = Instantiate(bullet[0], spawn, fireTransform.transform.rotation) as Rigidbody;
                     //TRIG USED HERE
                     // Sqrt(y^2 + (Sqrt(x^2 + z^2))^2)
                     shellInstance.velocity = new Vector3
                                                     (
-                                                        (player.transform.position.x - fireTransform.transform.position.x),
-                                                        (player.transform.position.y - fireTransform.transform.position.y) / 2,
-                                                        (player.transform.position.z - fireTransform.transform.position.z)
+                                                        (player.transform.position.x - shellInstance.transform.position.x),
+                                                        (player.transform.position.y - shellInstance.transform.position.y) / 2,
+                                                        (player.transform.position.z - shellInstance.transform.position.z)
                     );
                     //canSpawn = false;
                     break;
-                case BulletState.missile:
+                case Ammo.missile:
                     Debug.Log("Spawned Missile");
                     spawn = new Vector3(fireTransform.transform.position.x + i, fireTransform.transform.position.y, fireTransform.transform.position.z);
                     shellInstance = Instantiate(bullet[1], spawn, fireTransform.transform.rotation) as Rigidbody;
                     shellInstance.velocity = 1 * fireTransform.transform.forward;
                     canSpawn = false;
                     break;
-                case BulletState.bullet:
+                case Ammo.bullet:
                     Debug.Log("Spawned Bullet");
                     spawn = new Vector3(fireTransform.transform.position.x, fireTransform.transform.position.y, fireTransform.transform.position.z);
                     shellInstance = Instantiate(bullet[2], spawn, fireTransform.transform.rotation) as Rigidbody;
@@ -65,5 +79,12 @@ public class BossShooting : MonoBehaviour
             }
             savedTime = GameManager.GlobalTimer;
         }
+ 
     }
+    /*public void Fire(int AmmoType){
+               switch(shootingArangement){
+            case ShootingArangement.Gattling:
+                
+        }
+    }*/
 }
---
+++ SMCF2/Assets/Scripts/SinDraw.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinDraw : MonoBehaviour
+{
+    //Sets the position
+    public Vector3 pos;
+    //Speed the object moves at
+    public float Speed;
+    //Size that objects moves
+    public float Size;
+    //Multiplier along the X
+    public float xMulti;
+        //Multiplier along the y
+    public float yMulti;
+        //Multiplier along the z
+    public float zMulti;
+        //Offsets from 0,0,0 
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+    //Changes wether or not it calculated using Sin Cos or Tan
+    // Sin = 0
+    // Cos = 1
+    // Tan = 2
+    public int[] SinCosTan = new int[3];
+    void Update()
+    {
+        switch (SinCosTan[0])
+        {
+            case 0:
+                pos.x = xOffset + Mathf.Sin(Time.time * Speed * xMulti) * Size;
+                break;
+            case 1:
+                pos.x = xOffset + Mathf.Cos(Time.time * Speed * xMulti) * Size;
+                break;
+            case 2:
+                pos.x = xOffset + Mathf.Tan(Time.time * Speed * xMulti) * Size;
+                break;
+        }
+        switch (SinCosTan[1])
+        {
+            case 0:
+                pos.y = yOffset + Mathf.Sin(Time.time * Speed * yMulti) * Size;
+                break;
+            case 1:
+                pos.y = yOffset + Mathf.Cos(Time.time * Speed * yMulti) * Size;
+                break;
+            case 2:
+                pos.y = yOffset + Mathf.Tan(Time.time * Speed * yMulti) * Size;
+                break;
+        }
+        switch (SinCosTan[2])
+        {
+            case 0:
+                pos.z = zOffset + Mathf.Sin(Time.time * Speed * zMulti) * Size;
+                break;
+            case 1:
+                pos.z = zOffset + Mathf.Cos(Time.time * Speed * zMulti) * Size;
+                break;
+            case 2:
+                pos.z = zOffset + Mathf.Tan(Time.time * Speed * zMulti) * Size;
+                break;
+        }
+        gameObject.transform.position = pos;
+    }
+}
